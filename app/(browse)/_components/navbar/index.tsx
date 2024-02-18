7c5c53665cee7db4fb22b86cd3dfe9a0491371c8
sarthak18t <sarthaktailor@gmail.com>
2024-02-18 16:02:38 +0530

    navbar completed

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Logo from "./logo";
 import Search from "./search";
-
+import Actions from "./actions"
 const Navbar = () => {
   return (
     <nav className="fixed top-0 bg-[#252731] w-full h-20 flex items-center justify-between px-2 lg:px-4 z-[49] shadow-sm">
       <Logo/>
       <Search/>
+      <Actions/>
     </nav>
   );
 };
